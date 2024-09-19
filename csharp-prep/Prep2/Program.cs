52f59ab8204bb756dd16c7052ac35dfee000fd4e
e-arndt <earndt1701@gmail.com>
2024-09-19 14:50:54 -0700

    Complete final core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -11,48 +11,51 @@ namespace Grades
             Console.Write("What is your grade percent? ");
             string MyGrade = Console.ReadLine();
             int UserGrade = int.Parse(MyGrade);
+            string letter = "";
             Console.Clear();
 
-            if (UserGrade >= 90)
-            {
-                Console.Write("Your Letter Grade is A");
-            }
+                if (UserGrade >= 90)
+                {
+                    letter = "A";
+                }
 
-            else if (UserGrade >= 80)
-            {
-                Console.Write("Your Letter Grade is B");
-            }
+                else if (UserGrade >= 80)
+                {
+                    letter = "B";
+                }
 
-            else if (UserGrade >= 70)
-            {
-                Console.Write("Your Letter Grade is C");
-            }
+                else if (UserGrade >= 70)
+                {
+                    letter = "C";
+                }
 
-            else if (UserGrade >= 60)
-            {
-                Console.Write("Your Letter Grade is D");
-            }
-            
-            else if (UserGrade < 60)
-            {
-                Console.Write("Your Letter Grade is F");
-            }
+                else if (UserGrade >= 60)
+                {
+                    letter = "D";
+                }
+                
+                else if (UserGrade < 60)
+                {
+                    letter = "F";
+                }
 
-            if (UserGrade >= 70)
-            {
-                Console.WriteLine();
-                Console.Write("You passed the class, Congratulations!");
-            }
+        Console.WriteLine($"Your Letter Grade is {letter}.");
 
-            else if (UserGrade < 70)
-            {
-                Console.WriteLine();
-                Console.WriteLine("You did not pass the class.");
-                Console.WriteLine("Try again, ask a tutor for help.");
-            }
+                if (UserGrade >= 70)
+                {
+                    Console.WriteLine();
+                    Console.Write("You passed the class, Congratulations!");
+                }
 
-            Console.WriteLine();
-            Console.WriteLine();
+                else if (UserGrade < 70)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("You did not pass the class.");
+                    Console.WriteLine("Try again, ask a tutor for help.");
+                }
+
+        Console.WriteLine();
+        Console.WriteLine();
         }
     }
 }
