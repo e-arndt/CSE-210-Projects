eafe458e1d82baf64425cab4aed8d5a816f02105
e-arndt <earndt1701@gmail.com>
2024-09-18 22:44:01 -0700

    Add spacing to make code more readable

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,10 +7,13 @@ class Program
         Console.Clear();
         Console.Write("What is your first name? ");
         string fname = Console.ReadLine();
+
         Console.Write("What is your last name ? ");
         string lname = Console.ReadLine();
+
         Console.WriteLine();
         Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+        
         Console.WriteLine();
         Console.WriteLine();
     }
