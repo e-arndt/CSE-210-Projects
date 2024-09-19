858a288021ea1791746eae589f5a39c1844caef6
e-arndt <earndt1701@gmail.com>
2024-09-19 14:26:16 -0700

    Added a namespace

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,20 +1,23 @@
 using System;
 
-class Program
+namespace Bond
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        Console.Clear();
-        Console.Write("What is your first name? ");
-        string fname = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("What is your first name? ");
+            string fname = Console.ReadLine();
 
-        Console.Write("What is your last name ? ");
-        string lname = Console.ReadLine();
+            Console.Write("What is your last name ? ");
+            string lname = Console.ReadLine();
 
-        Console.WriteLine();
-        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+            Console.WriteLine();
+            Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
         
-        Console.WriteLine();
-        Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
     }
 }
