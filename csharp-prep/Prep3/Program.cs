a1897e367e2f0b6d2e37787977ac3c226afc57f6
e-arndt <earndt1701@gmail.com>
2024-09-19 21:58:36 -0700

    Completed 1st Core Requirement

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
 
-class Program
+namespace Grades
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int MagicNumber = 1;
+                int NumberGuess = 0;
+                Console.Clear();
+                Console.Write("What is the Magic Number? ");
+                MagicNumber = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                while (NumberGuess != MagicNumber)
+                {
+                    Console.Write("What is your guess? ");
+                    string MyGuess = Console.ReadLine();
+                    NumberGuess = int.Parse(MyGuess);
+
+                    if (NumberGuess > MagicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                        else if (NumberGuess < MagicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                        else
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"You guessed {MagicNumber} which is the Magic Number!");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                    }
+                }    
+        }
     }
 }
