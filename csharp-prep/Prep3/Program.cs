9bedde8397b08ea00e18381d96d641c361c88e6c
e-arndt <earndt1701@gmail.com>
2024-09-19 23:37:40 -0700

    Completed core + all Stretch Challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,40 +1,84 @@
 using System;
+using System.Threading;
 
-namespace Grades
+
+
+namespace Guess
 {
+
         internal class Program
         {
-            static void Main(string[] args)
+            private static void NumberGuess()
             {
-                int MagicNumber = 1;
-                int NumberGuess = 0;
-                Console.Clear();
-                Console.Write("What is the Magic Number? ");
-                MagicNumber = int.Parse(Console.ReadLine());
-                Console.Clear();
+                Random rndNum = new Random();
+                int MagicNumber = rndNum.Next(1, 250);
 
+                int NumberGuess = 0;
+                int GuessCount = 0;
+
+                Console.Clear();
+                Console.WriteLine("Guess the Magic Number! [1 - 250]");
+                Console.WriteLine();
+                Console.Write("Press 'Enter' when ready ");
+                Console.ReadLine();
+                Console.Clear();
+                
                 while (NumberGuess != MagicNumber)
                 {
+                    GuessCount++;
                     Console.Write("What is your guess? ");
                     string MyGuess = Console.ReadLine();
                     NumberGuess = int.Parse(MyGuess);
 
                     if (NumberGuess > MagicNumber)
-                    {
-                        Console.WriteLine("Lower");
-                    }
-                        else if (NumberGuess < MagicNumber)
-                    {
-                        Console.WriteLine("Higher");
-                    }
-                        else
-                    {
-                        Console.Clear();
-                        Console.WriteLine($"You guessed {MagicNumber} which is the Magic Number!");
-                        Console.WriteLine();
-                        Console.WriteLine();
-                    }
-                }    
+                        {
+                            Console.Write("Lower");
+                            Thread.Sleep(1000);
+                            Console.Clear();
+                        }
+                            else if (NumberGuess < MagicNumber)
+                        {
+                            Console.Write("Higher");
+                            Thread.Sleep(1000);
+                            Console.Clear();
+                        }
+                            else
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"You guessed the Magic Number {MagicNumber} in {GuessCount} tries!");
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            Console.WriteLine();
+                        }
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                char input;
+                do
+                {
+                    NumberGuess();
+
+                    Console.Write("Would you like to play again? [Y / N]");
+                    input = Convert.ToChar(Console.ReadLine());
+                }
+
+                while (input == 'y');
+
+                Console.WriteLine();
+                Console.WriteLine("Thanks for playing... Bye!");
+                Thread.Sleep(1250);
+                Console.Clear();
+            }
         }
-    }
 }
+                  
+        
+
+
+
+
+
+
+        
