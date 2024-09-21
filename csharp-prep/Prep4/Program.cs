6539720d71026b527b62f2296f67a0912a86845e
e-arndt <earndt1701@gmail.com>
2024-09-20 21:52:55 -0700

    Follow along with lesson

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+
+namespace Lists
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+        List<string> words = new List<string>();
+
+        words.Add("phone");
+        words.Add("keyboard");
+        words.Add("mouse");
+
+        Console.Clear();
+        Console.Write("Word count of the lists is: ");
+        Console.WriteLine(words.Count);
+        Console.WriteLine();
+
+        Console.WriteLine("Words in the lists are: ");
+        
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+            }
+        }
+
 }
