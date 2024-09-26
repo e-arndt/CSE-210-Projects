5e2c24f113af483534ef683b540c6e3568138bee
e-arndt <earndt1701@gmail.com>
2024-09-25 17:03:56 -0700

    Change resume name and data

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,21 +6,21 @@ class Program
     {
         // Instance 1 of job class and associated member variables
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._jobTitle = "Production Technician";
+        job1._company = "NorthWest Cable News";
+        job1._startYear = 2015;
+        job1._endYear = 2016;
 
         // Instance 2 of Job class and associated member variables
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
+        job2._jobTitle = "Studio Production Engineer";
+        job2._company = "King Broadcasting";
+        job2._startYear = 2016;
         job2._endYear = 2023;
 
         // Create new instance of Resume class
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose"; // Set member variable
+        myResume._name = "Eric Arndt"; // Set member variable
 
         // Add job data from both jobs to list
         myResume._jobs.Add(job1);
